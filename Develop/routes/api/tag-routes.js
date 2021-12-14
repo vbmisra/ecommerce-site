5125b97294cce6a6d2ca4ecca7441d41bca7d442
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-12-14 01:13:46 -0800

    updated tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,6 +6,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag, as: 'associated_product' }]
+    })
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -15,6 +23,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body)
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
