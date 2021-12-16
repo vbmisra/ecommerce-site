852d0046614c02cdd2cf09148c4759423eb9ebf0
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-12-15 23:27:07 -0800

    updated routes, server file and README

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -19,6 +19,14 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag, as: 'associated_product' }]
+    })
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
@@ -33,10 +41,39 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!tagData) {
+      res.status(404).json('No product found associated with this id')
+    }
+
+    res.status(200).json(tagData)
+  } catch(err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!tagData) {
+      res.status(404).json('No product found associated with this id')
+    }
+
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
